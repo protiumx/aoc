e9e5f4423cc118bc3b3a25a0d6ed394830275e5c
Brian Mayo <ioprotium@gmail.com>
2021-12-13 10:56:04 +0100

    feat: added rust solution to day 02

---
+++ aoc-2021/packages/aoc-rust/src/day_02.rs
@@ -0,0 +1,40 @@
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+
+fn part_one() -> i32 {
+    let data = BufReader::new(File::open("../../input/day_02.in").unwrap())
+        .lines()
+        .filter_map(|l| l.ok())
+        .fold((0_i32, 0_i32), |x_y, line| {
+            let data: Vec<&str> = line.split(" ").collect();
+            let val = data[1].parse::<i32>().unwrap();
+            match data[0] {
+                "forward" => (x_y.0 + val, x_y.1),
+                "up" => (x_y.0, x_y.1 - val),
+                "down" => (x_y.0, x_y.1 + val),
+                _ => x_y,
+            }
+        });
+    return data.0 * data.1;
+}
+
+fn part_two() -> i32 {
+    let data = BufReader::new(File::open("../../input/day_02.in").unwrap())
+        .lines()
+        .filter_map(|l| l.ok())
+        .fold((0_i32, 0_i32, 0_i32), |x_y_aim, line| {
+            let data: Vec<&str> = line.split(" ").collect();
+            let val = data[1].parse::<i32>().unwrap();
+            match data[0] {
+                "forward" => (x_y_aim.0 + val, x_y_aim.1 + x_y_aim.2 * val, x_y_aim.2),
+                "up" => (x_y_aim.0, x_y_aim.1, x_y_aim.2 - val),
+                "down" => (x_y_aim.0, x_y_aim.1, x_y_aim.2 + val),
+                _ => x_y_aim,
+            }
+        });
+    return data.0 * data.1;
+}
+pub fn show() {
+    println!("- Answer part one: {}", part_one());
+    println!("- Answer part two: {}", part_two());
+}
--- aoc-2021/packages/aoc-rust/src/main.rs
+++ aoc-2021/packages/aoc-rust/src/main.rs
@@ -1,6 +1,9 @@
 mod day_01;
+mod day_02;
 
 fn main() {
     println!("Day 01:");
     day_01::show();
+    println!("Day 02:");
+    day_02::show();
 }
