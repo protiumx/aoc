293c392a6806ac897d2ed8cf41d22ecad914a1cd
Brian Mayo <ioprotium@gmail.com>
2022-12-07 22:49:40 +0100

    fix answer text

--- aoc-2022/packages/aoc-go/main.go
+++ aoc-2022/packages/aoc-go/main.go
@@ -33,6 +33,6 @@ func main() {
 	fmt.Printf("day_06.2 answer: pairs overlapped %d\n", solutions.Day_06_02(string(input)))
 
 	input, _ = ioutil.ReadFile("../../input/day_07.in")
-	fmt.Printf("day_07.1 answer: start of packet at %d\n", solutions.Day_07_01(string(input)))
-	fmt.Printf("day_07.2 answer: pairs overlapped %d\n", solutions.Day_07_02(string(input)))
+	fmt.Printf("day_07.1 answer: total size of directories %d\n", solutions.Day_07_01(string(input)))
+	fmt.Printf("day_07.2 answer: size of directory to delete %d\n", solutions.Day_07_02(string(input)))
 }
