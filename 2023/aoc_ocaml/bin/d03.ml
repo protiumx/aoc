e05de01470fa01577e8dd4fc6834b7dd0ec4be87
Brian Mayo <ioprotium@gmail.com>
2023-12-09 22:28:02 +0000

    use Array.make_matrix

--- 2023/aoc_ocaml/bin/d03.ml
+++ 2023/aoc_ocaml/bin/d03.ml
@@ -1,21 +1,23 @@
 open Core
 
 let lines = AOC.read_lines "input/d03.in"
-let matrix = List.map lines ~f:String.to_list
-let rows, cols = AOC.matrix_size matrix
+let rows, cols = (List.length lines, String.length (List.hd_exn lines))
+let matrix : char array array = Array.make_matrix ~dimx:rows ~dimy:cols Char.min_value
 let in_bounds x y = AOC.matrix_in_bounds rows cols x y
 
 module IntSet = Stdlib.Set.Make (Int)
 module Hashtbl = Stdlib.Hashtbl
 
 let part_1 =
+  List.iteri lines ~f:(fun x line -> String.iteri line ~f:(fun y c -> matrix.(x).(y) <- c));
+
   let rec collect (x, y) acc has_part current_num =
     if x >= rows then acc
     else if y >= cols then
       let acc = if has_part then acc + current_num else acc in
       collect (x + 1, 0) acc false 0
     else
-      let current = AOC.matrix_nth matrix x y in
+      let current = matrix.(x).(y) in
       match Char.is_digit current with
       | true ->
           let current_num = (current_num * 10) + (int_of_char current - int_of_char '0') in
@@ -25,7 +27,7 @@ let part_1 =
                    let row, col = (x + i, y + j) in
                    if not (in_bounds row col) then valid
                    else
-                     match AOC.matrix_nth matrix row col with
+                     match matrix.(row).(col) with
                      | '.' | '0' .. '9' -> valid
                      | _ -> true)
           in
@@ -37,7 +39,8 @@ let part_1 =
   collect (0, 0) 0 false 0 |> Fmt.pr "@.Part 2: %d@."
 
 let part_2 =
-  let nums_map = Hashtbl.create (List.length matrix) in
+  let nums_map = Hashtbl.create (Array.length matrix) in
+  List.iteri lines ~f:(fun x line -> String.iteri line ~f:(fun y c -> matrix.(x).(y) <- c));
 
   let update_nums gears num =
     if num > 0 then
@@ -58,7 +61,7 @@ let part_2 =
       update_nums gears current_num;
       collect (x + 1, 0) IntSet.empty 0)
     else
-      let current = AOC.matrix_nth matrix x y in
+      let current = matrix.(x).(y) in
       let gears, current_num =
         match Char.is_digit current with
         | true ->
@@ -68,7 +71,7 @@ let part_2 =
                   let row, col = (x + i, y + j) in
                   if not (in_bounds row col) then op
                   else
-                    match AOC.matrix_nth matrix row col with
+                    match matrix.(row).(col) with
                     | '*' -> Some (row, col)
                     | _ -> op)
             in
