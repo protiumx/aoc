5be378e1337d9232e9e40e747d22788ef41baac6
Brian Mayo <ioprotium@gmail.com>
2024-12-01 00:27:49 +0000

    probably solved?

--- 2023/aoc_ocaml/bin/d24.ml
+++ 2023/aoc_ocaml/bin/d24.ml
@@ -3,10 +3,10 @@
 
 open Core
 
-let lines = AOC.read_lines "input/d24.in"
+let lines = AOC.read_lines "input/d24.test"
 
 module Hailstone = struct
-  (* represented as lines on a plane *)
+  (* lines in 3D *)
   type t = {
     x : int64;
     y : int64;
@@ -29,38 +29,50 @@ module Hailstone = struct
     in
     { x; y; z; vx; vy; vz; a = vy; b = Int64.neg vx; c = Int64.((vy * x) - (vx * y)) }
 
+  let position (hs : t) = function
+    | 0 -> hs.x
+    | 1 -> hs.y
+    | 2 -> hs.z
+
+  let velocity (hs : t) = function
+    | 0 -> hs.vx
+    | 1 -> hs.vy
+    | 2 -> hs.vz
+
   let intersects (hs1 : t) (hs2 : t) (low, high) =
     (* parallels *)
     if Int64.(hs1.a * hs2.b = hs1.b * hs2.a) then
       false
     else
-      let m = Float.of_int64 Int64.((hs1.a * hs2.b) - (hs2.a * hs1.b)) in
-      let x = Float.of_int64 Int64.((hs1.c * hs2.b) - (hs2.c * hs1.b)) /. m in
-      let y = Float.of_int64 Int64.((hs2.c * hs1.a) - (hs1.c * hs2.a)) /. m in
+      let determinant = Float.of_int64 Int64.((hs1.a * hs2.b) - (hs2.a * hs1.b)) in
+      let x = Float.of_int64 Int64.((hs1.c * hs2.b) - (hs2.c * hs1.b)) /. determinant in
+      let y = Float.of_int64 Int64.((hs2.c * hs1.a) - (hs1.c * hs2.a)) /. determinant in
       (* intersection should happen in the future *)
-      let inter =
-        Float.(low <= x && x <= high && low <= y && y <= high)
-        && List.for_all [ hs1; hs2 ] ~f:(fun h ->
-               Float.(
-                 (x - Float.of_int64 h.x) * Float.of_int64 h.vx >= 0.0
-                 && (y - Float.of_int64 h.y) * Float.of_int64 h.vy >= 0.0))
-      in
-      inter
+      Float.(low <= x && x <= high && low <= y && y <= high)
+      && List.for_all [ hs1; hs2 ] ~f:(fun h ->
+             Float.(
+               (x - Float.of_int64 h.x) * Float.of_int64 h.vx >= 0.0
+               && (y - Float.of_int64 h.y) * Float.of_int64 h.vy >= 0.0))
 end
 
 let hailstones = List.map lines ~f:Hailstone.make |> Array.of_list
 
 let part_1 =
-  (* let low = 7.0 in *)
-  (* let high = 27.0 in *)
-  let low = 200000000000000.0 in
-  let high = 400000000000000.0 in
+  let low = 7.0 in
+  let high = 27.0 in
+  (* let low = 200000000000000.0 in *)
+  (* let high = 400000000000000.0 in *)
   Array.foldi hailstones ~init:0 ~f:(fun i acc hs1 ->
       Array.fold (Array.slice hailstones 0 i) ~init:acc ~f:(fun acc' hs2 ->
           acc' + Bool.to_int (Hailstone.intersects hs1 hs2 (low, high))))
   |> Fmt.pr "@.Part 1: %d@."
 
-let part_2 = ()
+let part_2 =
+  (* search velocity in 1D *)
+  let search_velocity component =
+    Array.sort hailstones ~compare:(fun a b -> Int64.compare (Hailstone.velocity a component) (Hailstone.velocity b component));
+
+  in
 
 let () =
   part_1;
